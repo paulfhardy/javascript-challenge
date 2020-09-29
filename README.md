910d398e0310a68d86f30e33342a28d958d1bd15
Paul Hardy <paulfhardy@hotmail.com>
2020-09-29 16:10:07 -0700

    Documentation and organization of Level 1 and Level 2 folders completed

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # javascript-challenge
+
+## Please Note
+
+Both Level 1 and Level 2 functionality requirements have been completed for this challenge and links to both 
+are available here:
+
+Level 1: [Level 1: Automatic Table and Date search](UFO-Level-1/index.html)
+Level 2: [Level 2: Multiple Search Categories](UFO-Level-2/index.html)
+
+### Level 1: Automatic Table and Date Search (Required)
+
+* The StarterCode/index.html file provided was used to build the initial web page.
+
+* Upon page load, the full data set is loaded and displayed on the page.
+
+* Data for each column listed here and available in the dataset appears on the page `date/time`, `city`, `state`, `country`, `shape`, and `comment`.
+
+* Filter and Form Submit event listeners have been implemented in the app.ja to listen and respond by searching `date/time` column to find rows that match user input.  The table displays the rendered data accurately, after clearing any previously visible data.
+
+### Level 2: Multiple Search Categories (Optional)
+
+* Multiple input tags were added to the index.html, and the app was modified so that the user can set multiple filters and search for UFO sightings for any combination of the following criteria based on the table columns:
+
+  1. `date/time`
+  2. `city`
+  3. `state`
+  4. `country`
+  5. `shape`
+
+* If the user clears all of the filter criteria, then the full data set is displayed, as on original load of the page.
+
+---
--- UFO-Level-2/static/js/app.js
+++ UFO-Level-2/static/js/app.js
@@ -32,6 +32,14 @@ const runEnter = () => {
   // Prevent the page from refreshing on submit
   d3.event.preventDefault();
 
+  // Establish filtereData as an exact copy of the full data set
+  // The logic below will determine if the user has entered filter criteria
+  // in each of the input fields, if so the appropriate filter will be applied to
+  // filteredData, if not filteredData remains as is and is evaluated for the next input
+  // field until all fields are evaluated. Then the table is rebuilt for desplay with 
+  // exactly what the user requested.  If no filter criteria is entered the full data set 
+  // is displayed.
+
   // Make a copy of the tableData data set to filteredData.
   var filteredData = tableData.map(item => item );
 
@@ -42,6 +50,39 @@ const runEnter = () => {
       if (inputValue) {
         var filteredData = filteredData.filter(ufoRecord => ufoRecord.datetime === inputValue);
       }
+
+  // Select the city input element and get the raw HTML node
+  // Get the value property of the input element
+  let inputCityElement = d3.select("#city"), 
+       cityinputValue = inputCityElement.property("value");
+       if (cityinputValue) {
+        var filteredData = filteredData.filter(ufoRecord => ufoRecord.city === cityinputValue);
+      }
+
+  // Select the state input element and get the raw HTML node
+  // Get the value property of the input element
+  let inputStateElement = d3.select("#state"), 
+       stateinputValue = inputStateElement.property("value");
+       if (stateinputValue) {
+        var filteredData = filteredData.filter(ufoRecord => ufoRecord.state === stateinputValue);
+      }
+
+  // Select the country input element and get the raw HTML node
+  // Get the value property of the input element
+  let inputCountryElement = d3.select("#country"), 
+       countryinputValue = inputCountryElement.property("value");
+       if (countryinputValue) {
+        var filteredData = filteredData.filter(ufoRecord => ufoRecord.country === countryinputValue);
+      }
+
+  // Select the shape input element and get the raw HTML node
+  // Get the value property of the input element
+  let inputShapeElement = d3.select("#shape"), 
+       shapeinputValue = inputShapeElement.property("value");
+       if (shapeinputValue) {
+        var filteredData = filteredData.filter(ufoRecord => ufoRecord.shape === shapeinputValue);
+      }
+ 
 
   // Clear prior tr elements at tbody level (leave thead>tr as is)
   var trall = d3.selectAll("tbody>tr");
