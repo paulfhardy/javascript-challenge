bd76ecfe528981d6e81d3e6aa71c24b308d9f0a9
Paul Hardy <paulfhardy@hotmail.com>
2020-09-28 21:10:39 -0700

    Initial Page load with all data appearing on screen

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,4 +1,17 @@
 // from data.js
 var tableData = data;
-
+console.log(tableData);
 // YOUR CODE HERE!
+const tbody = d3.select("tbody");
+
+// Use d3 to update each cell's text with
+// UFO sighting data values 
+// (Date, City, State, Country, Shape, Duration, Comments)
+tableData.forEach(ufoReport => {
+   let row = tbody.append("tr");
+   Object.values(ufoReport).forEach(value => {
+     // Append a cell to the row for each value in the uforeport object
+     var cell = row.append("td");
+     cell.text(value);
+   });
+});
